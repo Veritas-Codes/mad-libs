306971b1f091eba54cc872c6b0a774e96dbf80ef
Rauha Rahkola <rrahkola@gmail.com>
2019-09-14 03:23:26 -0700

    adding a story

---
+++ pirate-story.py
@@ -0,0 +1,20 @@
+adj1 = input('Enter an adjective: ')
+name1 = input('Enter a name: ')
+verb1 = input('Enter a verb (past tense): ')
+adj2 = input('Enter an adjective: ')
+noun1 = input('Enter a plural noun: ')
+
+print(f"""
+Here is my story:
+
+
+
+There once was a feared pirate named {adj1.capitalize()} {name1.capitalize()}.
+The reason they were so feared was...
+
+
+
+... they used to {verb1} people with {adj2} {noun1}.
+
+Wow, scary!
+    """)
