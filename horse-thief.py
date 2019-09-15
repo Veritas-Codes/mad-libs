1f3faa847b92cc44953093050df017a25a69d326
landonDKnguyen <hnguyen@veritasschool.net>
2019-09-14 19:24:07 -0700

    i'm adding a story

---
+++ horse-thief.py
@@ -0,0 +1,14 @@
+adj2=input('enter a adjective:')
+verb2=input('enter a verb:')
+noun3=input('enter a noun:')
+
+
+
+
+
+print(f"""
+There was a {adjective}{noun} who {verb} a {noun} and then {verb} a {adjective}{noun}.
+""")
+
+
+
