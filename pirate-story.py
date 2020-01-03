568dfce1b2cb3a3757912319ff38ec4b5d0e2107
Rauha Rahkola <rrahkola@gmail.com>
2020-01-03 00:07:34 +0000

    now it works on raspberry pi.

--- pirate-story.py
+++ pirate-story.py
@@ -1,20 +1,28 @@
 adj1 = input('Enter an adjective: ')
 name1 = input('Enter a name: ')
-verb1 = input('Enter a verb (past tense): ')
+verb1 = input('Enter a verb (infinitive): ')
 adj2 = input('Enter an adjective: ')
 noun1 = input('Enter a plural noun: ')
 
-print(f"""
-Here is my story:
+#print(f"""
+#Here is my story:
+#
+#
+#
+#There once was a feared pirate named {adj1.capitalize()} {name1.capitalize()}.
+#The reason they were so feared was...
+#
+#
+#
+#... they used to {verb1} people with {adj2} {noun1}.
+#
+#Wow, scary!
+#    """)
 
+output = "\n" + "Here is my story: \n\n\n\n" + \
+  "There once was a feared pirate named " + adj1.capitalize() + " " + name1.capitalize() + ".\n" + \
+  "The reason they were so feared was... \n\n\n\n" + \
+  "... they used to " + verb1 + " people with " + adj2 + " " + noun1 + ".\n" + \
+  "\nWow, scary!"
 
-
-There once was a feared pirate named {adj1.capitalize()} {name1.capitalize()}.
-The reason they were so feared was...
-
-
-
-... they used to {verb1} people with {adj2} {noun1}.
-
-Wow, scary!
-    """)
+print(output)
